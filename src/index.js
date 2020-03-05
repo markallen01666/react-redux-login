7d1103b65a59d8e55f24e7b3588ebede0c51c9ce
Mark Allen <markallen.webdeveloper@gmail.com>
2020-03-05 10:35:07 +0000

    Added basic Redux store setup to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialState = {
+  signedIn: false,
+  userName: ""
+};
 
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "SIGNIN":
+      return Object.assign({}, state, {
+        signedIn: true,
+        userName: "Fred Bloggs"
+      });
+    case "SIGNOUT":
+      return Object.assign({}, state, {
+        signedIn: false,
+        userName: ""
+      });
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "DECREMENT" });
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
